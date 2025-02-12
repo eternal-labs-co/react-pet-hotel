f3e7b6a34960b5ab870363c8d4d78d7698a88310
Graham Thomas <graham@eternal.co>
2025-02-12 09:20:11 -0700

    Changes from PR

--- src/api/mockApi.ts
+++ src/api/mockApi.ts
@@ -1,16 +1,16 @@
 import { http, HttpResponse } from "msw";
 import { Owner, Pet, Toy } from "./types";
-import { PETS } from "./mockData";
+import { OWNERS, PETS, TOYS } from "./mockData";
 
 const handlers = [
   http.get<any, any, Pet[]>("/api/pets", () => {
     return HttpResponse.json(PETS);
   }),
   http.get<any, any, Owner[]>("/api/owners", () => {
-    return HttpResponse.json([]);
+    return HttpResponse.json(OWNERS);
   }),
   http.get<any, any, Toy[]>("/api/toys", () => {
-    return HttpResponse.json([]);
+    return HttpResponse.json(TOYS);
   }),
 ];
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "wouter";
-import PetList from "./PetList";
 
 function HomePage() {
   return (
